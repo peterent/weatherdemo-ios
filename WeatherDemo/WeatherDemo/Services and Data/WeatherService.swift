5f6cb05ac52355cc4604000ed9b76b82135d5ff6
Peter Ent <peterent@me.com>
2021-02-26 16:01:10 -0500

    Replaced Alamofire with URLSession + Combine.

--- WeatherDemo/WeatherDemo/Services and Data/WeatherService.swift
+++ WeatherDemo/WeatherDemo/Services and Data/WeatherService.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 import MapKit
-import Alamofire
+import Combine
 
 /**
  * Connects to the openweather.com REST API service and processes the results into data structures
@@ -21,27 +21,33 @@ class WeatherService {
     }
     
     private let apiKey = "<add your openweather.com API key here>"
+    
+    private var subscriptions = Set<AnyCancellable>()
         
     func getAllWeatherInfo(for location: CLLocationCoordinate2D, units: WeatherUnits, onComplete: @escaping ((WeatherData?)->Void)) {
         let lat = location.latitude
         let lon = location.longitude
-        AF.request("https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=minutely,alerts&units=\(units.rawValue)&appid=\(apiKey)").responseJSON { response in
-            if let error = response.error {
-                print("We have an error: \(error)")
-                onComplete(nil)
-            }
-            if let data = response.data {
-                do {
-                    let allWd = try JSONDecoder().decode(WeatherData.self, from: data)
-                    DispatchQueue.main.async {
-                        onComplete(allWd)
-                    }
-                } catch {
-                    print("JSON decoding error: \(error)")
+        
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=minutely,alerts&units=\(units.rawValue)&appid=\(apiKey)") else { return }
+        
+        URLSession.shared
+            .dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: WeatherData.self, decoder: JSONDecoder())
+            .sink { completion in
+                
+                if case .failure(let err) = completion {
+                    print("Retrieving data from weather service received this error: \(err)")
                     onComplete(nil)
                 }
+                
+            } receiveValue: { object in
+                
+                DispatchQueue.main.async {
+                    onComplete(object)
+                }
             }
-        }
+            .store(in: &subscriptions)
     }
     
 }
